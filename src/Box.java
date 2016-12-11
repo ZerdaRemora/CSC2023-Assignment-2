0b2c3de4438676ce169a8d4addf8e7647b92c65a
ZerdaRemora <james@zerdaremora.tech>
2016-12-11 22:19:44 +0000

    Method stubs added for Truck and Box.

--- src/Box.java
+++ src/Box.java
@@ -1,6 +1,49 @@
 /**
- * Represents a box to be loaded into a truck.
+ * Represents a box to be loaded into a {@link Truck Truck}.
+ * Each Box stores a pointer to the Box directly on top of it,
+ * much like a linked list.
  */
 public class Box
 {
+    private int width;
+    private int height;
+    private Box aboveBox;
+
+    public Box(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        this.aboveBox = null;   // By default, has no box above it.
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public Box getAboveBox()
+    {
+        return aboveBox;
+    }
+
+    public void setAboveBox(Box aboveBox)
+    {
+        // Validate before setting.
+        // Also, bear in mind that the above box cannot be changed
+        // so maybe have a boolean flag that checks if it has been changed previously.
+        this.aboveBox = aboveBox;
+    }
+
+    public boolean canFitBox(Box box)
+    {
+        // Check if a box can fit on this box.
+        // i.e. length/width less/equal to this
+        // and no other boxes on this box.
+        return false;
+    }
 }
--- src/Truck.java
+++ src/Truck.java
@@ -1,6 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents a truck in the Truck Loading Problem.
+ * Boxes lying on the Truck bed are stored in the Box ArrayList in this
+ * object, whereas a higher box is stored in the {@link Box Box} object that is directly underneath
+ * it.
  */
 public class Truck
 {
+    private static final int TRUCK_WIDTH = 2000;
+    private static final int TRUCK_HEIGHT = 1000;
+    private int remainingWidth;
+    private int remainingHeight;
+    private List<Box> boxes;    // Box List to hold all bottom-level boxes.
+
+    public Truck()
+    {
+        // Initialise Truck so that it's remaining space is it's default capacity.
+        this.remainingWidth = TRUCK_WIDTH;
+        this.remainingHeight = TRUCK_HEIGHT;
+        boxes = new ArrayList<Box>();
+    }
+
+    public int getRemainingWidth()
+    {
+        return remainingWidth;
+    }
+
+    public int getRemainingHeight()
+    {
+        return remainingHeight;
+    }
+
+    public List<Box> getBoxes()
+    {
+        return boxes;
+    }
+
+    public void addBox(Box box)
+    {
+        this.boxes.add(box);
+    }
+
+    public boolean canFit(Box box)
+    {
+        // Check if passed box can fit in the truck.
+        // Check last accessed pile in NFTLP or every pile
+        // in FFTLP.
+        return false;
+    }
 }
